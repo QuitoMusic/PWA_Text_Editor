1a5ccc7b38d8d8beb7d08278819a318f1a671841
Francisco Gonzalez <f86gonzalez@outlook.com>
2023-08-29 10:50:40 -0400

    Added CSS loaders and babel to webpack.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -51,11 +51,25 @@ module.exports = () => {
 
     ],
 
-    module: {
-      rules: [
-// TODO: Add CSS loaders and babel to webpack.
-        
-      ],
+module: {
+  rules: [
+
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
     },
-  };
+    {
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+        },
+      },
+    },
+  ],
+},
+};
 };
